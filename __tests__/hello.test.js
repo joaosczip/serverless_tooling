46d25db340ca11c5fbccbc4ecaf382f97f7a3d34
João Guilherme <joao.sczip@hotmail.com>
2020-05-12 18:32:40 -0300

    chore: add test command to package.json

---
+++ __tests__/hello.test.js
@@ -0,0 +1,19 @@
+"use strict";
+
+const mod = require("./..handler");
+
+const jestPlugin = require("serverless-jest-plugin");
+const lambdaWrapper = jestPlugin.lambdaWrapper;
+const wrapped = lambdaWrapper.wrap(mod, { handler: "hello" });
+
+describe("hello", () => {
+  beforeAll((done) => {
+    done();
+  });
+
+  it("implement tests here", () => {
+    return wrapped.run({}).then((response) => {
+      expect(response).toBeDefined();
+    });
+  });
+});
